2d9464285eec012d36fccd5ea680aa54c85e854c
jobanjot-grewal <23bia50005@cuchd.in>
2025-10-02 22:51:25 +0530

    Create App.js

---
+++ LabMST/App.js
@@ -0,0 +1,12 @@
+import MyForm from './labMST';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My React Form</h1>
+      <MyForm />
+    </div>
+  );
+}
+
+export default App;
